99c6db8a23f352ef67ed9b9398350107a5f07f32
goldenmember94 <katenev.ar@yandex.ru>
2020-08-06 15:27:43 +0300

    добавлены коментарии к файлу webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path')
+const path = require('path') //встроенный модуль позволяющий комфортно работать с путями
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: './index.js',
+    mode: 'development', //собираем все в режиме разработки
+    entry: './index.js', //входной файл приложения
     output: {
         filename: 'boundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    } //куда вебпак собирает проект, filename - название файла, path - путь файла
 }
