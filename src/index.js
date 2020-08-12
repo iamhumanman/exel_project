38981d6c99ddfc0d6ae7b13d37bdddbe3e47eb93
goldenmember94 <katenev.ar@yandex.ru>
2020-08-12 18:53:01 +0300

    creating class structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './scss/index.scss'
+import { Excel } from '@/components/excel/Excel'
 
-console.log('Тест системы')
+const excel = new Excel('#app', {
+  components: []
+})
+
+console.log('Excel', excel)
