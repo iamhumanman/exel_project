87470432d3180206471cb6010694ca62e12f5a85
goldenmember94 <katenev.ar@yandex.ru>
2020-09-07 19:36:37 +0300

    +

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import {Header} from '@/components/header/Header'
 import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
-import { createStore } from './core/createStore'
+import { createStore } from '@core/createStore'
 import { rootReducer } from './redux/rootReducer'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, {tableTitle: 'My table excel'})
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
