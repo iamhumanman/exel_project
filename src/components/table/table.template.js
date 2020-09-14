17797fe6640fd70e52edd36b322123f4e7918105
goldenmember94 <katenev.ar@yandex.ru>
2020-09-14 14:54:46 +0300

    рендер состояния

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -7,32 +7,30 @@ const CODES ={
 }
 
 //функция создания колонки
-function toColumn(col, index) {
+function toColumn({col, index, width}) {
   return `
-  <div class="column" data-type="resizable" data-col="${index}">
+  <div class="column" data-type="resizable" 
+  data-col="${index}" style="width: ${width}">
   ${col}
   <div class="col-resize" data-resize="col"></div>
   </div>
   `
 }
 
-//функия создания ячейки
-// function toCell(row, col) {
-//   return `
-//   <div class="cell" contenteditable data-col="${col}"></div>
-//   `
-// }
+const DEFAULT_WIDTH = 120
 
 //функция создания ячейки с замыканием
-function toCell(row) {
+function toCell(state, row) {
   return function(_, col) {
+    const width = getWidth(state.colState, col)
     return `
     <div 
     class="cell"
     contenteditable
     data-col="${col}"
     data-type="cell"
-    data-id="${row}:${col}"
+    data-id="${row}:${col}" 
+    style="width: ${width}"
     ></div>
     `
   }
@@ -57,23 +55,39 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
+export function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
+function withWidthFrom(state) {
+  return function(col, index) {
+    return {
+      col, index, width: getWidth(state.colState, index)
+    }
+  }
+}
+
 //функция генерации таблицы
-export function createTable(rowsCount = 15) {
+export function createTable(rowsCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
+      .map(withWidthFrom(state))
       .map(toColumn)
+      // .map((col, index) => {
+      //   const width = getWidth(state.colState, index)
+      //   return toColumn(col, index, width)
+      // })
       .join('')
   rows.push(createRow(null, cols))
 
   for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        // .map((_, col) => toCell(row, col))
-        .map(toCell(row))
+        .map(toCell(state, row))
         .join('')
     rows.push(createRow(row + 1, cells))
   }
