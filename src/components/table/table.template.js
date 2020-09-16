8565d862340e45066d0357851f6242a01bb4c702
goldenmember94 <katenev.ar@yandex.ru>
2020-09-16 15:04:25 +0300

    размер строчек

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -18,11 +18,20 @@ function toColumn({col, index, width}) {
 }
 
 const DEFAULT_WIDTH = 120
+const DEFAULT_HEIGHT = 24
+
+export function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
+export function getHeight(state, index) {
+  return (state[index] || DEFAULT_HEIGHT) + 'px'
+}
 
 //функция создания ячейки с замыканием
 function toCell(state, row) {
   return function(_, col) {
-    const width = getWidth(state.colState, col)
+    const width = getWidth(state, col)
     return `
     <div 
     class="cell"
@@ -37,10 +46,16 @@ function toCell(state, row) {
 }
 
 //функция создания строки
-function createRow(index, content) {
+function createRow(index, content, state) {
   const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+  const height = getHeight(state, index)
   return `
-  <div class="row" data-type="resizable">
+  <div 
+  class="row" 
+  data-type="resizable" 
+  data-row="${index}"
+  style="height: ${height}"
+  >
     <div class="row-info">
     ${index ? index: ''}
     ${resize}
@@ -55,9 +70,6 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function getWidth(state, index) {
-  return (state[index] || DEFAULT_WIDTH) + 'px'
-}
 
 function withWidthFrom(state) {
   return function(col, index) {
@@ -82,14 +94,14 @@ export function createTable(rowsCount = 15, state = {}) {
       //   return toColumn(col, index, width)
       // })
       .join('')
-  rows.push(createRow(null, cols))
+  rows.push(createRow(null, cols, {}))
 
   for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        .map(toCell(state, row))
+        .map(toCell(state.colState, row))
         .join('')
-    rows.push(createRow(row + 1, cells))
+    rows.push(createRow(row + 1, cells, state.rowState))
   }
 
   return rows.join('')
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,11 +3,13 @@ import { TABLE_RESIZE } from "./types"
 //pure function
 export function rootReducer(state, action) {
   let prevState
+  let field
   switch (action.type) {
     case TABLE_RESIZE:
+      field = action.data.type === 'col' ? 'colState': 'rowState'
       prevState = state.colState || {}
       prevState[action.data.id] = action.data.value
-      return {...state, colState: prevState} //id, value
+      return {...state, [field]: prevState} //id, value
     default: return state
   }
 }
