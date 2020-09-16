36453e7e5c168f1ef20c5f8dd7e973810bef5403
goldenmember94 <katenev.ar@yandex.ru>
2020-09-16 20:18:50 +0300

    +

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,15 +1,20 @@
-import { TABLE_RESIZE } from "./types"
+import { CHANGE_TEXT, TABLE_RESIZE } from "./types"
 
 //pure function
 export function rootReducer(state, action) {
   let prevState
   let field
+  console.log('ACTION: ', action);
   switch (action.type) {
     case TABLE_RESIZE:
       field = action.data.type === 'col' ? 'colState': 'rowState'
       prevState = state.colState || {}
       prevState[action.data.id] = action.data.value
       return {...state, [field]: prevState} //id, value
+    case CHANGE_TEXT:
+      prevState = state['dataState'] || {}
+      prevState[action.data.id] = action.data.value
+      return {...state, currentText: action.data.value, dataState: prevState}
     default: return state
   }
 }
