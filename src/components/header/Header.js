bd2a8714411750be8b17e88a904efa080a226b78
goldenmember94 <katenev.ar@yandex.ru>
2020-09-17 20:33:34 +0300

    оптимизация  с debounce

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import { ExcelComponent } from "@core/ExcelComponent";
 import { defaultTitle } from "../../constants";
 import { $ } from "../../core/dom";
+import { debounce } from "../../core/utils";
 import { changeTitle } from "../../redux/actions";
 
 export class Header extends ExcelComponent {
@@ -12,6 +13,10 @@ export class Header extends ExcelComponent {
       listeners: ['input'],
       ...options,
     })
+  }
+
+  prepare() {
+    this.onInput = debounce(this.onInput, 300)
   }
 
   toHTML() {
