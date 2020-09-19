036a9e3954ce4ba78cd38ce727f5da051e77f8a7
goldenmember94 <katenev.ar@yandex.ru>
2020-09-19 13:04:25 +0300

    создание главной страницы

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,33 +1,25 @@
 import { Page } from "../core/Page";
 import { $ } from '@core/dom'
+import { createRecordsTable } from "./dashboard.function";
 
 
 export class DashboardPage extends Page {
   getRoot() {
+    const nov = Date.now().toString()
     return $.create('div', 'db').html(`
     <div class="db">
     <div class="db__header">
-    <h1>Excel таблицы</h1>
+    <h1>Excel панель управления</h1>
     </div>
     <div class="db__new">
     <div class="db__view">
-    <a href="#" class="db__create">
+    <a href="#excel/${nov}" class="db__create">
     Новая <br /> таблица
     </a>
     </div>
     </div>
     <div class="db__table db__view">
-    <div class="db__list-header">
-    <span>Название</span>
-    <span>Дата открытия</span>
-    </div>
-    <ul class="db__list">
-    <li class="db__record">
-    <a href="#" >Таблица номер 1</a>
-    <strong>11.08.2020</strong>
-    </li>
-    </ul>
-    </div>
+    ${createRecordsTable()}
     </div>
     `)
   }
