90bc3dec7ecba901376de77f359ebbe07277c446
goldenmember94 <katenev.ar@yandex.ru>
2020-09-21 12:42:21 +0300

    добавление даты открытия

--- src/pages/dashboard.function.js
+++ src/pages/dashboard.function.js
@@ -6,7 +6,10 @@ function toHTML(key) {
   return `
   <li class="db__record">
   <a href="#excel/${id}" >${model.title}</a>
-  <strong>11.08.2020</strong>
+  <strong>
+  ${new Date(model.openedDate).toLocaleDateString()}
+  ${new Date(model.openedDate).toLocaleTimeString()}
+  </strong>
   </li>
   `
 }
